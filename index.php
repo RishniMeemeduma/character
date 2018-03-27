5587b0d351c1628601353b5e9d8c68efd9770917
RishniMeemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-03-27 05:32:44 +0530

    v

--- index.php
+++ index.php
@@ -87,27 +87,13 @@
       ]);
 
       // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
-      //   $helper = $fb->getRedirectLoginHelper();
+         $helper = $fb->getRedirectLoginHelper();
       //   $helper = $fb->getJavaScriptHelper();
       //   $helper = $fb->getCanvasHelper();
       //   $helper = $fb->getPageTabHelper();
-
-      try {
-        // Get the \Facebook\GraphNodes\GraphUser object for the current user.
-        // If you provided a 'default_access_token', the '{access-token}' is optional.
-        $response = $fb->get('/me', '{access-token}');
-      } catch(\Facebook\Exceptions\FacebookResponseException $e) {
-        // When Graph returns an error
-        echo 'Graph returned an error: ' . $e->getMessage();
-        exit;
-      } catch(\Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
-      }
-
-      $me = $response->getGraphUser();
-      echo 'Logged in as ' . $me->getName();
+         $permissions=['user_name','user_likes'];
+         $loginUrl =$helper->getLoginUrl('https://findcharacter.herokuapp.com/login-callback.php',$permissions);
+         echo '<a href="'.$loginUrl.'">Log in with Facebook!</a>'
     ?>
 		<div >
 			<h1 class="topic">Find your Koombiyo Character</h1>
