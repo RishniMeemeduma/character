b61b473068a1541a21d0b4c3b605b5c880f9a72e
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-03-28 15:28:59 +0530

    Update result.php

--- result.php
+++ result.php
@@ -39,26 +39,25 @@ $k = array_rand($characters);
 $v = $characters[$k];
 
 print_r($v);
-
 // Create image
-$image = new \NMC\ImageWithText\Image(dirname(__FILE__) . '/source.jpg');
+$image = new \NMC\ImageWithText\Image('source.jpg');
 
-// Add styled text to image
+// Add text to image
 $text1 = new \NMC\ImageWithText\Text('Thanks for using our image text PHP library!', 3, 25);
 $text1->align = 'left';
 $text1->color = 'FFFFFF';
-$text1->font = dirname(__FILE__) . '/Ubuntu-Medium.ttf';
+$text1->font = 'fonts/Ubuntu-Medium.ttf';
 $text1->lineHeight = 36;
 $text1->size = 24;
 $text1->startX = 40;
 $text1->startY = 40;
 $image->addText($text1);
 
-// Add another styled text to image
-$text2 = new \NMC\ImageWithText\Text('No, really, ', 1, 30);
+// Add more text to image
+$text2 = new \NMC\ImageWithText\Text('No, really, thanks!', 1, 30);
 $text2->align = 'left';
 $text2->color = '000000';
-$text2->font = dirname(__FILE__) . '/Ubuntu-Medium.ttf';
+$text2->font = 'fonts/Ubuntu-Medium.ttf';
 $text2->lineHeight = 20;
 $text2->size = 14;
 $text2->startX = 40;
@@ -66,7 +65,9 @@ $text2->startY = 140;
 $image->addText($text2);
 
 // Render image
-$image2=render(dirname(__FILE__) . '/destination.jpg');
-$image->render(dirname(__FILE__) . '/destination.jpg');
+$image->render('destination.jpg');
+// Render image
+$image2=render('destination.jpg');
+
 echo "<img src='$image2'>";
 ?>
