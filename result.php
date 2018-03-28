4214bf13c3c2e5e974ebc302a8e4fd8e6d4e447d
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-03-28 10:44:25 +0530

    Update result.php

--- result.php
+++ result.php
@@ -10,7 +10,7 @@ $fb = new Facebook\Facebook([
 
 try {
   // Returns a `Facebook\FacebookResponse` object
-  $response = $fb->get('/me?fields=id,name',$_SESSION['fb_access_token']);
+  $response = $fb->get('/me?fields=id,name,profile_pic',$_SESSION['fb_access_token']);
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
   echo 'Graph returned an error: ' . $e->getMessage();
   exit;
