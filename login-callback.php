25550e0814a428bec8d859db1bbed8a44407ccec
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-03-28 09:21:32 +0530

    Update login-callback.php

--- login-callback.php
+++ login-callback.php
@@ -9,11 +9,11 @@ $helper = $fb->getRedirectLoginHelper();
 
 try {
   $accessToken = $helper->getAccessToken();
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
+} catch(vendor\facebook\graph-sdk\src\Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
   echo 'Graph returned an error: ' . $e->getMessage();
   exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
+} catch(vendor\facebook\graph-sdk\src\Facebook\Exceptions\FacebookSDKException $e) {
   // When validation fails or other local issues
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
   exit;
