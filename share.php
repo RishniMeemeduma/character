b67efac407bb09344d8dd8a3f9b7040bc041b526
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-08 11:07:44 +0530

    Create share.php

---
+++ share.php
@@ -0,0 +1,1 @@
+<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&layout=button_count&size=large&mobile_iframe=true&appId=178728269598594&width=84&height=28" width="84" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
