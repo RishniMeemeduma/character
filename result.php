853dd0b1758ad92b6df1839da6307d9c64a10f63
RishniMeemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-08 13:18:59 +0530

    c

--- result.php
+++ result.php
@@ -93,7 +93,9 @@ if($v=='Ananda'){
 }
 if($v=='Priyantha'){
  echo" <img src='img/priyantha.jpg'>";
-}?>
+}
+echo"<img src='destination.jpg'>"
+?>
     <div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true">
       <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
     </div>
