57ddfb75dc534f8ec00269c1bdb01d9340fa318b
RishniMeemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-08 11:47:58 +0530

    n

--- result.php
+++ result.php
@@ -120,6 +120,7 @@ $image->addText($text2);
 // Render image
 $image->render('destination.jpg');
 ?>
+<!DOCTYPE html>
 <html>
 <head>
   <title>Your Website Title</title>
@@ -129,6 +130,17 @@ $image->render('destination.jpg');
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="Your Website Title" />
   <meta property="og:description"   content="Your description" />
-  <meta property="og:image"         content='https://findcharacter.herokuapp.com/destination.jpg" />
+  <meta property="og:image"         content="https://findcharacter.herokuapp.com/destination.jpg" />
 </head>
 <body>
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=178728269598594&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+</body>
+</html>
