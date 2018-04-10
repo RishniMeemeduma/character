0d1ca5ddc62c900195b4839915f8516c695c6244
RishniMeemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-10 21:10:08 +0530

    c

--- result.php
+++ result.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset='utf-8'>
-  <title>Your Koombiyo Character</title>
-     <meta property="og:url"           content="https://findcharacter.herokuapp.com/result.php" />
-  <meta property="og:type"          content="website" />
-  <meta property="og:title"         content="Your Website Title" />
-  <meta property="og:description"   content="Your description" />
-  <meta property="og:image"         content="https://findcharacter.herokuapp.com/destination.jpg" />
-  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-  <style type="text/css">
-  .container{background: url(img/ant.jpg) no-repeat top fixed;position: relative;background-size: cover;height: 800px}
-  .u_pic{width: 150px;height: 200px;position: absolute;display: block;top: 100px;line-height: 0.9em;}
-  .c_pic{width: 150px;height: 200px;position: absolute;right: 30px;top:100px;display: block;}
-  .u_name{position:absolute; color: #fff;top: 300px;}
-  .c_name{position:absolute; color: #fff;top: 300px;right:100px;}
-  h1{position: absolute;color: rgba(255,255,255,0.6);left: calc(50% - 150px);}
-  </style>
-</head>
-<body>
-  <?php
+<?php
 
 session_start();
 require 'vendor/autoload.php';
@@ -97,6 +75,28 @@ if($v=='Priyantha'){
 }
 echo"<img src='destination.jpg'>"
 ?> 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <title>Your Koombiyo Character</title>
+     <meta property="og:url"           content="https://findcharacter.herokuapp.com/result.php" />
+  <meta property="og:type"          content="website" />
+  <meta property="og:title"         content="Your Website Title" />
+  <meta property="og:description"   content="Your description" />
+  <meta property="og:image"         content="https://findcharacter.herokuapp.com/destination.jpg" />
+  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+  <style type="text/css">
+  .container{background: url(img/ant.jpg) no-repeat top fixed;position: relative;background-size: cover;height: 800px}
+  .u_pic{width: 150px;height: 200px;position: absolute;display: block;top: 100px;line-height: 0.9em;}
+  .c_pic{width: 150px;height: 200px;position: absolute;right: 30px;top:100px;display: block;}
+  .u_name{position:absolute; color: #fff;top: 300px;}
+  .c_name{position:absolute; color: #fff;top: 300px;right:100px;}
+  h1{position: absolute;color: rgba(255,255,255,0.6);left: calc(50% - 150px);}
+  </style>
+</head>
+<body>
+  
   <section class="container">
     <h1>Your Koombiyo Character is</h1>
     <section class="user">
