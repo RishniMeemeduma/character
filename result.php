0995b81b62b956287fac4b8741272cae0fa894cf
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-19 13:58:23 +0530

    Update result.php

--- result.php
+++ result.php
@@ -57,17 +57,38 @@ $k = array_rand($characters);
 $v = $characters[$k];
 
 /*print_r($v);*/
-/*
+
 $ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,"http://koombiyoweb.000webhostapp.com/result_new.php");
-curl_setopt($ch,CURLOPT_POST,1);
-curl_setopt($ch,CURLOPT_POSTFIELDS,"uname='.$user["name"].'&cname='.$v.'");
 
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-$servier_output=curl_exec($ch);
-$server_output=json_decode($server_output,true);
+curl_setopt($ch, CURLOPT_URL,"http://koombiyoweb.000webhostapp.com/result_new.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "uname=testneame&cname=testimage");
 
-echo var_dump($server_output,true);*/
+// in real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+//          http_build_query(array('postvar1' => 'value1')));
+
+// receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);  //response
+
+$server_output =json_decode($server_output, true);
+
+
+
+//echo var_dump($server_output);
+
+$myimg=$server_output['image'];
+?>
+
+<img src="data:image/png;base64, <?php echo $myimg;?>" alt="Red dot" />
+
+    <?php
+
+
+curl_close ($ch);
 ?> 
  
     <section class="share">
