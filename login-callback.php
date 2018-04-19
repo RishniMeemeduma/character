988eba80c90d8f052eeb5b7f96e3eebc7c436cfc
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-19 08:37:31 +0530

    Update login-callback.php

--- login-callback.php
+++ login-callback.php
@@ -72,33 +72,7 @@ $_SESSION['fb_access_token'] = (string) $accessToken;
 // You can redirect them to a members-only page.
 //header('Location: https://example.com/members.php');
 ?>
-<?php
-//
-// A very simple PHP example that sends a HTTP POST to a remote site
-//
 
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL,"koombiyoweb.000webhostapp.com/image.php");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "postvar1=value1&postvar2=value2&postvar3=value3");
-
-// in real life you should use something like:
-// curl_setopt($ch, CURLOPT_POSTFIELDS, 
-//          http_build_query(array('postvar1' => 'value1')));
-
-// receive server response ...
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$server_output = curl_exec ($ch);
-
-curl_close ($ch);
-
-// further processing ....
-if ($server_output == "OK") {return $ch } else { print_r("Image not created"). }
-
-?>
 <!DOCTYPE HTML>
 <html>
 	<head>
