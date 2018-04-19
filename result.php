386f3a543983fe011718c82eabe98cc141aa6406
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-20 04:55:18 +0530

    Update result.php

--- result.php
+++ result.php
@@ -105,23 +105,23 @@ $myimg=$server_output['image'];
 <head>
   <meta charset='utf-8'>
   <title>Your Koombiyo Character</title>
-     <meta property="og:url"           content="https://findcharacter.herokuapp.com/result.php" />
+     <meta property="og:url"           content="https://koombiyoweb.000webhostapp.com/result_new.php" />
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="Your Website Title" />
   <meta property="og:description"   content="Your description" />
-  <meta property="og:image"         content="<img src='data:image/png;base64, <?php echo $myimg;?>' alt='Red dot' />" />
+  <meta property="og:image"         content="https://koombiyoweb.000webhostapp.com/destination.jpg" />
   <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
   
 </head>
 <body>
-   <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=178728269598594&autoLogAppEvents=1';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    </script>
+   <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=178728269598594&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <img src="data:image/png;base64, <?php echo $myimg;?>" alt="Red dot" />
 
     <?php
@@ -133,9 +133,7 @@ curl_close ($ch);
     <section class="share">
       <p>Share your result with facebook.</p>
     
-    <div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true">
-      <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
-    </div>
+    <div class="fb-share-button" data-href="https://koombiyoweb.000webhostapp.com/result_new.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fkoombiyoweb.000webhostapp.com%2Fresult_new.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
     </section>
 
 </body>
