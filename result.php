f913d577b0195b995903e3a87d35c99c3befbe5f
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-26 10:28:02 +0530

    Update result.php

--- result.php
+++ result.php
@@ -137,7 +137,7 @@ curl_close ($ch);
     <section class="share">
       <p>Share your result with facebook.</p>
     
-<div class="fb-share-button"<div class="fb-like" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div> data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>    </section>
+<div class="fb-like" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div> data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>    </section>
 
 </body>
 </html>
