5d1df0a7d16dcff78c18b382f73beb1b94a79193
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-26 10:41:41 +0530

    Update result.php

--- result.php
+++ result.php
@@ -108,9 +108,8 @@ $myimg=$server_output['image'];
 <head>
   <meta charset='utf-8'>
   <title>Your Koombiyo Character</title>
-      <meta property="og:url"        content="https://findcharacter.herokuapp.com/result.php" />
+      <meta property="og:url"           content="https://findcharacter.herokuapp.com/result.php" />
       <meta property="og:type"          content="website" />
-      <meta property="fb:app_id"        content="178728269598594" />
       <meta property="og:title"         content="Your Website Title" />
       <meta property="og:description"   content="Your description" />
       <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_image.jpg" />
@@ -137,8 +136,10 @@ curl_close ($ch);
     <section class="share">
       <p>Share your result with facebook.</p>
     
-<div class="fb-like" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
-      </section>
+      <div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true">
+      <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
+      </div>
+     </section>
 
 </body>
 </html>
