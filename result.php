e87492cacfe7191aa408ba44b7c7431f92cead78
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-26 10:45:12 +0530

    Update result.php

--- result.php
+++ result.php
@@ -112,7 +112,7 @@ $myimg=$server_output['image'];
       <meta property="og:type"          content="website" />
       <meta property="og:title"         content="Your Website Title" />
       <meta property="og:description"   content="Your description" />
-      <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_image.jpg" />
+      <meta property="og:image"         content="https://findcharacter.herokuapp.com/source.jpg" />
       <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
   
 </head>
