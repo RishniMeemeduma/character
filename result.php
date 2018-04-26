67340a1d3e8be3cc6d0a2a29d7daf3f530b64790
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-26 09:45:06 +0530

    Update result.php

--- result.php
+++ result.php
@@ -100,7 +100,8 @@ $server_output =json_decode($server_output, true);
 //echo var_dump($server_output);
 
 $myimg=$server_output['image'];
-<img src=$myimg>
+
+<img src="data:image/png;base64, <?php echo $myimg;?>" alt="Red dot" />
 ?>
 <!DOCTYPE html>
 <html>
