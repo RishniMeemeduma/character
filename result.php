80ac1b3612f09e231b5d208f134a192e10e0393e
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-28 16:42:25 +0530

    Update result.php

--- result.php
+++ result.php
@@ -123,24 +123,15 @@ curl_close ($ch);
 </head>
 <body>
 <div id="fb-root"></div>
-<script>
-   window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '178728269598594',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.2'
-    });
-  };
-
-  
-  (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = '//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=178728269598594&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
+<script>(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=178728269598594"; // appId must be valid
+fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+<fb:share-button type="box_count">
+</fb:share-button>
 
     <section class="share">
       <p>Share your result with facebook.</p>
