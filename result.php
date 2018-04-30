b944dab505cf6d2ee592976374410af4137fc766
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-04-30 16:18:10 +0530

    Update result.php

--- result.php
+++ result.php
@@ -110,24 +110,12 @@ curl_close ($ch);
 <html>
 <head>
   <title>Your Koombiyo Character</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:url"           content="https://findcharacter.herokuapp.com/" />
-        <meta property="og:type"          content="article" />
-        <meta property="og:title"         content="Find your koombiyo character" />
-        <meta property="og:description"   content="Find your koombiyo character" />
-        <meta property="og:image"         content="https://findcharacter.herokuapp.com/ant.jpg" />
+  
 </head>
 <body>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=178728269598594&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
   
-<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div></body>
+<div class="fb-share-button" data-href="" class="fb-xfbml-parse-ignore">Share</a></div></body>
 </html>
 
 
