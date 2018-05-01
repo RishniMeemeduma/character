ab09b808c1ed5fea97fbf198a8df2c38c5acea75
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-01 21:19:51 +0530

    Update result.php

--- result.php
+++ result.php
@@ -10,7 +10,15 @@
   <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_image.jpg" />
 </head>
 <body>
-   <div id="fb-root"></div>
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=178728269598594&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+   <!--<div id="fb-root"></div>
     <script>(function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
@@ -18,7 +26,7 @@
       js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=178728269598594&autoLogAppEvents=1';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
-    </script>
+    </script>-->
   <?php
 
 session_start();
@@ -126,9 +134,10 @@ $myimg=$server_output['image'];
  <?php
 curl_close ($ch);
 ?>
-<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true">
+<!--<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true">
       <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
-</div>
+</div>-->
+  <div class="fb-share-button" data-href="https://apps.facebook.com/koombiyoweb" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fapps.facebook.com%2Fkoombiyoweb&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
 </html>
 
 
