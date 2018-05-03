f3ee111b2aa8f216ff011c895216ecf3ed6885aa
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-03 21:18:54 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -8,7 +8,7 @@ session_start();
 <head>
   <title></title>
   <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-   <meta property="og:url"           content="https://findcharacter.herokuapp.com/result_new.php" />
+   <meta property="og:url"           content="https://findcharacter.herokuapp.com" />
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="Your Website Title" />
   <meta property="og:description"   content="Your description" />
@@ -68,7 +68,7 @@ curl_close ($ch);
 /***********************************************************************************************/
 ?>
 
-<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result_new.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult_new.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
 </body>
 </html>
 
