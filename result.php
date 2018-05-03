5568ab77ce630179b43ed945d1cea509c8898f5e
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-03 19:21:31 +0530

    Update result.php

--- result.php
+++ result.php
@@ -1,49 +1,4 @@
-<?php
 
-session_start();
-require 'vendor/autoload.php';
-
-$fb = new Facebook\Facebook([
-  'app_id' => '178728269598594',
-  'app_secret' => '34759706ffb61f4b9add1dae533ca766',
-  'default_graph_version' => 'v2.2',
-  ]);
-
-try {
-  // Returns a `Facebook\FacebookResponse` object
-  $response = $fb->get('/me?fields=id,name,picture',$_SESSION['fb_access_token']);
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-
-$user = $response->getGraphUser();
-
-/*echo 'Name: ' . $user['name'];*/
-$u_id=$user['id'];
-  $u_name=$user['name'];
-$picture=$user['picture'];
-$pc = json_decode($picture);
-//echo $pc->url;
-
-/*echo "<img src='$pc->url'>";*/
-//var_dump($pc);
-/*function array_kshift($characters){
-  list($k)=array_keys($characters);
-  $r=array($k=>$characters[$k]);
-  unset($characters[$k]);
-  return $r;
-
-}*/
-
-$characters= array("Hiruni","Priyantha","Adikari","Ananda","Jude","Thenne","Jehan Fernando","Maldeniya","Dog of Maldeniy","Tiran","Ostin aiya","Sudu nangi" );
-$k = array_rand($characters);
-$v = $characters[$k];
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -69,7 +24,55 @@ $v = $characters[$k];
     </script>
   
 
-  <?php
+<?php
+/*****************Facebook sdk***************************************************/
+session_start();
+require 'vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => '178728269598594',
+  'app_secret' => '34759706ffb61f4b9add1dae533ca766',
+  'default_graph_version' => 'v2.2',
+  ]);
+
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('/me?fields=id,name,picture',$_SESSION['fb_access_token']);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$user = $response->getGraphUser();
+/***********************************************************************************/
+/*************************************Get User Informations from facebook*************/        
+/*echo 'Name: ' . $user['name'];*/
+$u_id=$user['id'];
+  $u_name=$user['name'];
+$picture=$user['picture'];
+$pc = json_decode($picture);
+//echo $pc->url;
+
+/*echo "<img src='$pc->url'>";*/
+//var_dump($pc);
+/*function array_kshift($characters){
+  list($k)=array_keys($characters);
+  $r=array($k=>$characters[$k]);
+  unset($characters[$k]);
+  return $r;
+
+}*/
+/****************************************************************************************/
+/********************* Randomly select Character form php********************************/        
+$characters= array("Hiruni","Priyantha","Adikari","Ananda","Jude","Thenne","Jehan Fernando","Maldeniya","Dog of Maldeniy","Tiran","Ostin aiya","Sudu nangi" );
+$k = array_rand($characters);
+$v = $characters[$k];
+/****************************************************************************************/
+
+/*****************************Using Curl Send and get the Image from webhost*****************/
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,"http://koombiyoweb.000webhostapp.com/result_new.php");
@@ -99,11 +102,13 @@ $myimg=$server_output['image'];
   <img src="http://www.koombiyocharacter.me/joined_image<?php echo $u_id ?>.jpg">
  <?php
 curl_close ($ch);
+/***********************************************************************************************/
 ?>
-<!--<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result.php" data-layout="button_count" data-size="large" data-mobile-iframe="true">
-      <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
-</div>-->
-<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/index.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Findex.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div></html>
+
+<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/index.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Findex.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
+</div>
+</body>
+</html>
 
 
 
