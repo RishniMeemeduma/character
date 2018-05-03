fcef16f594beec21db1198841e85eb250bd05533
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-03 22:03:03 +0530

    Update login-callback.php

--- login-callback.php
+++ login-callback.php
@@ -91,7 +91,7 @@ $picture=$user['picture'];
 $pc = json_decode($picture);
 $_SESSION['u_id']=(string) $u_id;
 $_SESSION['u_name']=(string) $u_name;
-header("Location:https://findcharacter.herokuapp.com/result_new.php");
+
 //echo $pc->url;
 /*echo "<img src='$pc->url'>";*/
 //var_dump($pc);
