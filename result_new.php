67a23d64247adaec9d32db772eecd98974f9219c
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-05 19:38:40 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -9,7 +9,7 @@ $u_name=$_SESSION['u_name'];
 <head>
   <title></title>
   <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-    <meta property="og:url"          content="https://findcharacter.herokuapp.com/index.php" />
+    <meta property="og:url"          content="https://findcharacter.herokuapp.com/result_new.php" />
     <meta property="og:type"          content="website" />
     <meta property="og:title"         content="Find your koombiyo Character" />
     <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_images/joined_image<?php echo $u_id ?>.jpg" />
@@ -76,8 +76,7 @@ curl_close ($ch);
 /***********************************************************************************************/
 ?>
 
-<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/index.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Findex.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
-  </div>
+<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result_new.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult_new.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>  </div>
   <div class="new-request"><a href="https://findcharacter.herokuapp.com/index.php">Find your answer from here</a>
   </body>
   </html>
