a148f3767b819c7cb66dc637e9a20f7d9fde28fb
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-05 11:56:02 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -15,7 +15,13 @@ $u_name=$_SESSION['u_name'];
     <meta property="og:description"   content="Koombiyo Is a famous teledrama.Do you like to find out your koombiyo character." />
     <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_image<?php echo $u_id ?>.jpg" />
   <style>
-    img{position:fixed;left:50%;top:50%;}
+    img {display: block;
+         margin-left: auto;
+         margin-right: auto;
+        }
+    .fb-share-button fb_iframe_widget fb_iframe_widget_fluid{
+        margin-left: 47%;
+        }
     </style>
 </head>
 <body>
