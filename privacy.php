56de934e42f39f41781a9ef9da7d3a6af0050ec5
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-06 19:07:25 +0530

    Update privacy.php

--- privacy.php
+++ privacy.php
@@ -3,7 +3,8 @@ echo "author:Rishni Meemeduma";
 echo "Server:Heroku and ooowebhost";
 echo "PHP Version:7";
 echo "Used:Github,Sublimetext,Github desktop";
-
+echo "Login via facebook";
+echo "Using this application you can findout your koombiyo character.This is a facebook app.We are getting your facebook id,name,picture form your facebook profile."
 
 
 
