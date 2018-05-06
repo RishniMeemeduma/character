865c39f6e744a7a22745d293458c54fac167da36
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-06 20:39:10 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -38,7 +38,7 @@ $u_name=$_SESSION['u_name'];
           transform-origin: top left;
           position: absolute !important;
           top: 162px;
-          left: 61%;
+          left: 71%;
 
         }
     .new-request{
@@ -48,7 +48,7 @@ $u_name=$_SESSION['u_name'];
         /* margin-right: 30%; */
         /* margin-right: 28%; */
         /* width: 171%; */
-        padding: 12px 35px;
+        padding: 12px 20px;
         background: rgba(12, 159, 213, 0.78);
         color: #fff;
         text-align: center;
@@ -66,7 +66,7 @@ $u_name=$_SESSION['u_name'];
         {
          position: absolute;
          display: inline;
-         left: 61%;
+         left: 71%;
          top: 47%;
         }
     </style>
