560198840091db2a9245302b85babfc724960337
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-07 09:50:27 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -13,7 +13,7 @@ $time=time();
     <meta property="og:type"          content="website" />
     <meta property="og:title"         content="Find your koombiyo Character" />
     <meta property="og:description"   content="Koombiyo is a famous teledarama and you may like to check which character in koombiyo looks like you." />
-    <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_images/joined_image<?php echo $u_id?>.jpg" />
+   <?php echo '<meta property="og:image"         content="http://www.koombiyocharacter.me/joined_images/joined_image$u_id.jpg" />'?>
   <style>
     body{
         background:url(https://thumbs.dreamstime.com/b/shiny-silver-foil-texture-background-decor-white-grey-metallic-85153852.jpg) no-repeat center;
