2d1036c14460680a1a113690d7dec2a0c840a414
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-07 10:42:24 +0530

    Update create_image.php

--- create_image.php
+++ create_image.php
@@ -4,7 +4,7 @@ session_start();
 $u_id=$_SESSION['u_id'];
 $u_name=$_SESSION['u_name'];  
 $time=time();
-$_SESSION['time'] =(string) $time
+$_SESSION['time'] =(string) $time;
 /********************* Randomly select Character form php********************************/        
 $characters= array("Hiruni","Priyantha","Adikari","Anjana","Meesha","Ananda","Jude","Thenne","Jehan","Maldeniya","Dog of Maldeniy","Tiran","Ostin aiya","Sudu nangi" );
 $k = array_rand($characters);
