01670faac211fbe90c050b55fb4f7fb56488147a
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-09 12:24:20 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -1,11 +1,35 @@
 <?php
-session_start();
+/*session_start();
 $u_id=$_GET['u_id'];
 $u_name=$_SESSION['u_name'];  
-$time=$_SESSION['time'];
+$time=$_SESSION['time'];*/
 
-$og_url="http://www.koombiyocharacter.me/joined_images/joined_image$u_id.jpg";
+//$og_url="http://www.koombiyocharacter.me/joined_images/joined_image$u_id.jpg";
+$fb = new Facebook\Facebook([
+  'app_id' => '{app-id}',
+  'app_secret' => '{app-secret}',
+  'default_graph_version' => 'v2.2',
+  ]);
 
+$linkData = [
+  'link' => 'http://www.example.com',
+  'message' => 'User provided message',
+  ];
+
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->post('/me/feed', $linkData, '{access-token}');
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$graphNode = $response->getGraphNode();
+
+echo 'Posted with id: ' . $graphNode['id'];
 
 ?>
 <!DOCTYPE html>
