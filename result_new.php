7bc6f982d5f5b1e0810deb1c41d689eb69b44078
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-10 13:32:13 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -6,32 +6,6 @@ $u_name=$_SESSION['u_name'];
 $time=$_SESSION['time'];
 
 $og_url="http://www.koombiyocharacter.me/joined_images/joined_image$u_id.jpg";
-/*$fb = new Facebook\Facebook([
-   'app_id' => '178728269598594', // Replace {app-id} with your app id
-  'app_secret' => '34759706ffb61f4b9add1dae533ca766',
-  'default_graph_version' => 'v2.2',
-  ]);
-
-$linkData = [
-  'link' => 'https://findcharacter.herokuapp.com',
-  'message' => 'User provided message',
-  ];
-
-try {
-  // Returns a `Facebook\FacebookResponse` object
-  $response = $fb->post('/me/feed', $linkData, $_SESSION['fb_access_token']);
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-
-$graphNode = $response->getGraphNode();
-
-//echo 'Posted with id: ' . $graphNode['id'];*/
-
 ?>
 
 <!DOCTYPE html>
