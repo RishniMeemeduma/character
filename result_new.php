dc37d410acff8ec1624f9c0bd535249aa4361837
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-12 09:03:28 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -9,7 +9,7 @@
 		    <meta property="og:type"          content="website" />
 		    <meta property="og:title"         content="Find your koombiyo Character" />
 		    <meta property="og:description"   content="Koombiyo is a famous teledarama and you may like to check which character in koombiyo looks like you." />
-		    <!--<meta property="og:image"         content="http://www.koombiyocharacter.me/joined_images/joined_image967209046767984.jpg" />-->
+		    <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_images/joined_image967209046767984.jpg" />-->
 		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
   <style>
     body{
