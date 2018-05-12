c47870a1ef79625a9b92dcd0bd4ed7b1abb88465
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-12 10:52:04 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -5,12 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $u_id=$_SESSION['u_id'];
 $u_name=$_SESSION['u_name'];  
 $time=$_SESSION['time'];
-
-
 $og_url="http://www.koombiyocharacter.me/joined_images/joined_image$u_id.jpg";
-
-
-
 ?>     
   
 <!DOCTYPE HTML>
@@ -19,12 +14,12 @@ $og_url="http://www.koombiyocharacter.me/joined_images/joined_image$u_id.jpg";
 		<meta charset="utf-8">
 		<title>Find your Character</title>
 		  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-		    <!--<meta property="fb:app_id"		content="178728269598594" />
+		    <meta property="fb:app_id"		content="178728269598594" />
 		    <meta property="og:url"          content="https://findcharacter.herokuapp.com/index.php" />
 		    <meta property="og:type"          content="website" />
 		    <meta property="og:title"         content="Find your koombiyo Character" />
 		    <meta property="og:description"   content="Koombiyo is a famous teledarama and you may like to check which character in koombiyo looks like you." />
-		    <meta property="og:image"         content="http://www.koombiyocharacter.me/joined_images/joined_image967209046767984.jpg" />-->
+		    <meta property="og:image"         content="<?php echo $og_url ?>" />
 		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
   <style>
     body{
@@ -113,8 +108,16 @@ $myimg=$server_output['image'];*/
 //curl_close ($ch);
 /***********************************************************************************************/
 ?>
-
-  <div id="share_button" class="fb-share-button"><a href="share.php">Share</a></div>  
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=178728269598594&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+  
+  <div id="share_button" class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result_new.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult_new.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
   <div class="new"><a class="new-request" href="https://findcharacter.herokuapp.com/index.php">Click here to Find your answer</a></div>
 	
 
