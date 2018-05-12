e9c30f37fe2d1fc6207c3e3a95a11fa0c85f3afd
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-12 17:15:05 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -32,9 +32,12 @@ $timing=$_SESSION['timing'];
 	
         }
    	 .image {
-         
+         	margin-top: 5%;
         }
-	  .img{
+	img{
+		margin-left: 13%;
+		margin-right: auto;
+		display: block;
 	  }
    	.fb-share-button{
 		  transform: scale(1.5);
@@ -79,21 +82,21 @@ $timing=$_SESSION['timing'];
     
 </head>
 <body>
-<div class="image">
-  <img src="<?php echo $img ?>">
-</div>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=178728269598594&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-  
+<div class="container">
+	<div class="image">
+	  <img src="<?php echo $img ?>">
+	</div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=178728269598594&autoLogAppEvents=1';
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
 	<div class="fb-share-button" data-href="https://findcharacter.herokuapp.com/result_new.php?img=<?php echo $img ?>" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult_new.php%3Fimg%3D%253C%253Fphp%2Becho%2B%2524img%2B%253F%253E&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
 	<div class="new"><a class="new-request" href="https://findcharacter.herokuapp.com/index.php">f.aula .y,u n,uqo@</a></div>
-	
+</div>	
 
   </body>
   </html>
