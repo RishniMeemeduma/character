96213280ee2f7fa1b9007e517507b4a24d169a89
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-12 16:50:28 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -32,9 +32,7 @@ $timing=$_SESSION['timing'];
 	
         }
     img {
-         margin-top: 80px;
-         position: relative;
-         margin-left: 35px;
+         
         }
     .fb-share-button{
           transform: scale(1.5);
@@ -45,19 +43,12 @@ $timing=$_SESSION['timing'];
 
         }
     .new-request{
-        display: block;
-        margin-top: 30%;
-        padding: 12px 20px;
+        
         background: rgba(12, 159, 213, 0.78);
         color: #fff;
         text-align: center;
         font-size: 24px;
          font-family: araliya;
-        }
-    
-    .new-request:hover{
-        color:#fff;
-        text-decoration:none;
         }
      .new
         {
@@ -86,49 +77,9 @@ $timing=$_SESSION['timing'];
     
 </head>
 <body>
-
-
-<?php
-/********************* Randomly select Character form php********************************/        
-/*$characters= array("Hiruni","Priyantha","Adikari","Anjana","Meesha","Ananda","Jude","Thenne","Jehan","Maldeniya","Dog of Maldeniy","Tiran","Ostin aiya","Sudu nangi" );
-$k = array_rand($characters);
-$v = $characters[$k];*/
-/****************************************************************************************/
-
-/*****************************Using Curl Send and get the Image from webhost*****************/
-/*$ch = curl_init();
-      
-curl_setopt($ch, CURLOPT_URL,"http://koombiyoweb.000webhostapp.com/result_new.php");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,"uname=$u_name&cname=$v&id=$u_id");
-
-// in real life you should use something like:
-// curl_setopt($ch, CURLOPT_POSTFIELDS, 
-//          http_build_query(array('postvar1' => 'value1')));
-
-// receive server response ...
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$server_output = curl_exec ($ch);  //response
-
-$server_output =json_decode($server_output, true);
-
-
-
-//echo var_dump($server_output);
-
-$myimg=$server_output['image'];*/
-
-
-?>
-<!--<img src="data:image/png;base64, <?php //echo $myimg;?>" alt="Red dot" />-->
 <div class="image">
   <img src="<?php echo $img ?>">
 </div>
- <?php
-//curl_close ($ch);
-/***********************************************************************************************/
-?>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
