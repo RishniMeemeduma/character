5b38b5338a8912d5e6051f61afb1cfcea4c20f25
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-13 10:16:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -98,7 +98,26 @@
 					<center>
 						<img src="ant.jpg">
 						<h1 class="topic">Find your Koombiyo Character</h1>
-						<a href="'. htmlspecialchars($loginUrl) .'" class="login_to_fb">Log in with Facebook!</a
+						<?php
+						    session_start();
+							  require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
+
+						      $fb = new \Facebook\Facebook([
+							'app_id' => '178728269598594',
+							'app_secret' => '34759706ffb61f4b9add1dae533ca766',
+							'default_graph_version' => 'v2.2',
+							//'default_access_token' => '{access-token}', // optional
+						      ]);
+
+						      // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+							 $helper = $fb->getRedirectLoginHelper();
+						      //   $helper = $fb->getJavaScriptHelper();
+						      //   $helper = $fb->getCanvasHelper();
+						      //   $helper = $fb->getPageTabHelper();
+							 $permissions=['email'];
+							 $loginUrl =$helper->getLoginUrl('https://findcharacter.herokuapp.com/login-callback.php',$permissions);
+							 echo '<a href="'. htmlspecialchars($loginUrl) .'" class="login_to_fb">Log in with Facebook!</a>'
+						    ?>
 					</center>
 				</div>
       			</div>
