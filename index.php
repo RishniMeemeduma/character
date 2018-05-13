6c06c0f884f5afe13f8213efa8a13f7b08d464e2
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-13 13:35:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Find your Character</title>
-		<meta name="viewport" content="content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 		<style type='text/css'>
 			    
