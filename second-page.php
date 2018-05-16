790e5ce3ee3b4f7c1c013c4873dd751c5fca9d8e
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-05-16 13:09:03 +0530

    Update second-page.php

--- second-page.php
+++ second-page.php
@@ -116,6 +116,16 @@ $timing=$_SESSION['timing'];
           } 
 
 		</style>
+		
+	<!--**************************Google adsense advertisements**************************-->
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		  (adsbygoogle = window.adsbygoogle || []).push({
+		    google_ad_client: "ca-pub-7327566083992660",
+		    enable_page_level_ads: true
+		  });
+		</script>
+	
 	</head>
 	<body>
    
