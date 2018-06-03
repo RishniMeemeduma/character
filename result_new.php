a9353d7494756b173d80f704a77987186189cca3
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-06-03 09:00:50 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -107,32 +107,56 @@ $timing=$_SESSION['timing'];
 			  fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 	</script>
+	    
+	     <nav class="navbar navbar-default navbar-fixed-top small">
+		    <div class="container" >
+		      <div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1">
+			  <span class="sr-only">Toggle navigation</span>
+			  <span class="icon-bar"></span>
+			  <span class="icon-bar"></span>
+			  <span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="https://www.srifunapp.com"><img src="" alt="CreativeApp">
+			</a>
+		      </div>
+		       <div id="navbar1" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+			  <li><a href="https://findcharacter.herokuapp.com/">Home</a></li>
+			  <li><a href="https://findcharacter.herokuapp.com/about.php">About</a></li>
+			  <!--<li><a href="https://www.facebook.com/v2.8/dialog/oauth?client_id=676289399391580&state=67149cfb92121c08730dbf1c5d13a1b9&response_type=code&sdk=php-sdk-6.0-dev&redirect_uri=https%3A%2F%2Fwww.srifunapp.com%2Ffb%2Flogin.php&scope=email%2Cuser_friends%2Cuser_photos"><span class="glyphicon glyphicon-user"></span> <strong>Login</strong></a></li>
+			--></ul>
+		      </div>
+		    
+    		  </div>
+   
+ 		 </nav>
 	
-<div class="container">
-	<div class="row">
-		<div id="image" class="col-lg-8 col-md-8 col-sm-8">
-		<center>
-		  <img class="img-responsive" src="<?php echo $img ?>">
-		</center>
+		<div class="container">
+			<div class="row">
+				<div id="image" class="col-lg-8 col-md-8 col-sm-8">
+				<center>
+				  <img class="img-responsive" src="<?php echo $img ?>">
+				</center>
+				</div>
+
+				<div id="share" class="col-lg-3 col-md-3 col-sm-3">
+				<center>
+				<div data-href="https://findcharacter.herokuapp.com/result_new.php?img=<?php echo $img ?>" data-mobile-iframe="true">
+					<a class="fb-xfbml-parse-ignore"  onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'); return false;" 
+					   href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult_new.php%3Fimg=<?php echo $img?>&src=sdkpreparse" >
+					<img src="img/Facebook-share-button.png"></a></div>
+				</center>
+				</div>
+			</div>
+			<div class="row" style="margin-bottom:15px">
+				<div id="new" class="col-lg-9 col-md-9 col-sm-9">
+					<center>
+					<a class="new-request" href="https://findcharacter.herokuapp.com/index.php">Thdf. prsf;;a fu;kska n,d.uq</a>
+					</center>
+				</div>
+			</div>
 		</div>
-			
-		<div id="share" class="col-lg-3 col-md-3 col-sm-3">
-		<center>
-		<div data-href="https://findcharacter.herokuapp.com/result_new.php?img=<?php echo $img ?>" data-mobile-iframe="true">
-			<a class="fb-xfbml-parse-ignore"  onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'); return false;" 
-			   href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffindcharacter.herokuapp.com%2Fresult_new.php%3Fimg=<?php echo $img?>&src=sdkpreparse" >
-			<img src="img/Facebook-share-button.png"></a></div>
-		</center>
-		</div>
-	</div>
-	<div class="row" style="margin-bottom:15px">
-		<div id="new" class="col-lg-9 col-md-9 col-sm-9">
-			<center>
-			<a class="new-request" href="https://findcharacter.herokuapp.com/index.php">Thdf. prsf;;a fu;kska n,d.uq</a>
-			</center>
-		</div>
-	</div>
-</div>
 
   </body>
   </html>
