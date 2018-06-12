3a49321692429a46c0fb21b393487ef9bd559a18
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-06-12 05:32:54 +0530

    Update second-page.php

--- second-page.php
+++ second-page.php
@@ -140,7 +140,7 @@ $timing=$_SESSION['timing'];
 			  <span class="icon-bar"></span>
 			  <span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="https://www.srifunapp.com"><img src="" alt="CreativeApp">
+			<a class="navbar-brand" href=""><img src="" alt="CreativeApp">
 			</a>
 		      </div>
 		       <div id="navbar1" class="navbar-collapse collapse">
