3faac1f2a55bfd48b98c0f101fce8bd236e0a3dc
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-06-17 18:38:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Find your Character</title>
+		<title>Thd l+ôfhda tfla ljqre jf.ao@</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 		<style type='text/css'>
