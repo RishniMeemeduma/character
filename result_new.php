02e057823c5fa72e352b24fce01d48311e060f82
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-06-26 19:38:48 +0530

    Update result_new.php

--- result_new.php
+++ result_new.php
@@ -16,6 +16,11 @@ $timing=$_SESSION['timing'];
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 	<meta property="fb:app_id"		content="178728269598594" />
 	<!--<meta property="og:url"          content="https://findcharacter.herokuapp.com/result_new.php" />-->
 	<meta property="og:type"          content="website" />
