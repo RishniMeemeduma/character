2eab3439d42458f3ca114e77d2e535fae3af289a
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2018-06-26 19:39:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 		<title>FunApp-What is your koombiyo character.</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<style type='text/css'>
 			    
 		.topic{
